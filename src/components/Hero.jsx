8326079c2be7de5768bb3ee400e5067bbe382e0a
Selfmade20 <tebogo1686@gmail.com>
2024-12-03 22:05:17 +0200

    Updating information

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import {HERO_CONTENT} from '../constants';
-import profilePic from '../assets/kevinRushProfile.png';
+import profilePic from '../assets/myPhoto.jpg';
 import { motion } from 'framer-motion';
 
 
@@ -15,7 +15,7 @@ const container = (delay) => ({
 
 const Hero = () => {
   return (
-    <div className="border-b border-neutral-900 pb-4 lg:mg-35">
+    <div className="border-b border-neutral-900 pb-4 lg:mg-100">
         <div className="flex flex-wrap">
             <div className="w-full lg:w-1/2">
             <div className="flex flex-col items-center lg:items-start">
@@ -41,7 +41,7 @@ const Hero = () => {
             </div>
             </div>
             <div className='w-full lg:w-1/2 lg:p-8'>
-                <div className='flex justify-center'>
+                <div className='flex justify-center border-radius: 50%'>
                     <motion.img
                     initial ={{x: 100, opacity: 0}}
                     animate = {{x: 0, opacity: 1}}
